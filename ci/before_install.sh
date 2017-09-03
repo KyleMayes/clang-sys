b99f341972bc1c7f16ec757651841cac3e391651
Kyle Mayes <kyle@mayeses.com>
2017-09-03 12:14:34 -0400

    Cache LLVM downloads in Travis CI

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,4 +1,10 @@
 set -e
+pushd ~
+
+# Workaround for Travis CI macOS bug (https://github.com/travis-ci/travis-ci/issues/6307)
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    rvm get head || true
+fi
 
 function llvm_version_triple() {
     if [ "$1" == "3.5" ]; then
@@ -19,18 +25,27 @@ function llvm_version_triple() {
 function llvm_download() {
     export LLVM_VERSION_TRIPLE=`llvm_version_triple ${LLVM_VERSION}`
     export LLVM=clang+llvm-${LLVM_VERSION_TRIPLE}-x86_64-$1
+    export LLVM_DIRECTORY="$HOME/.llvm/${LLVM}"
 
-    wget http://llvm.org/releases/${LLVM_VERSION_TRIPLE}/${LLVM}.tar.xz
-    mkdir llvm
-    tar xf ${LLVM}.tar.xz -C llvm --strip-components=1
+    if [ -d "${LLVM_DIRECTORY}" ]; then
+        echo "Using cached LLVM download for ${LLVM}..."
+    else
+        wget http://llvm.org/releases/${LLVM_VERSION_TRIPLE}/${LLVM}.tar.xz
+        mkdir -p "${LLVM_DIRECTORY}"
+        tar xf ${LLVM}.tar.xz -C "${LLVM_DIRECTORY}" --strip-components=1
+    fi
 
-    export LLVM_CONFIG_PATH=`pwd`/llvm/bin/llvm-config
+    export LLVM_CONFIG_PATH="${LLVM_DIRECTORY}/bin/llvm-config"
 }
 
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
     llvm_download linux-gnu-ubuntu-14.04
-    export LD_LIBRARY_PATH=`pwd`/llvm/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH="${LLVM_DIRECTORY}/lib":$LD_LIBRARY_PATH
 else
     llvm_download apple-darwin
-    export DYLD_LIBRARY_PATH=`pwd`/llvm/lib:$DYLD_LIBRARY_PATH
+    cp "${LLVM_DIRECTORY}/lib/libclang.dylib" /usr/local/lib/libclang.dylib
+    export DYLD_LIBRARY_PATH="${LLVM_DIRECTORY}/lib":$DYLD_LIBRARY_PATH
 fi
+
+popd
+set +e
