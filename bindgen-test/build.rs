679e63930b400c503ebd31536f12f9a0034ba1fe
Kyle Mayes <kyle@mayeses.com>
2024-01-03 05:59:34 -0500

    Fix bindgen deprecation notice

--- bindgen-test/build.rs
+++ bindgen-test/build.rs
@@ -8,7 +8,7 @@ fn main() {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindgen::Builder::default()
         .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings!")
         .write_to_file(out.join("bindings.rs"))
